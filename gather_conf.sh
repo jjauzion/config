59bab550d3347a4cb8e92bd3064a100775fd449b
jjauzion <jjauzion@student.42.fr>
2020-01-08 14:29:57 +0100

    auto filter out line with KEY from .zshrc

--- gather_conf.sh
+++ gather_conf.sh
@@ -3,5 +3,11 @@
 cp ~/.vimrc .
 cp -r ~/.vim/ftplugin .
 cp -r ~/.vim/templates .
-cp ~/.zshrc .
 cp ~/.ideavimrc .
+sed '/KEY/d' ~/.zshrc > ./.zshrc
+deleted_line=$(sed -n '/KEY/p' ~/.zshrc)
+if [ ! -z "$deleted_line" ]
+then
+	echo "Automatic deletion of the following lines with KEY from .zshrc:"
+	echo $deleted_line
+fi
